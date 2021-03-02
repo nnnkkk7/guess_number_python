a0d011f1b09cb576818e07901fcee78fd65c345b
nnnkkk7 <kurodanaoki0711pana@gmail.com>
2021-03-02 10:07:21 +0900

    guess the number

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+
+def guess(x):
+    random_num = random.randint(1, x)
+    guess = 0
+    while guess != random_num:
+          guess = int(input(f'input your number between 1 and {x}: '))
+          if guess < random_num:
+            print("low number!!!!")
+          elif guess > random_num:
+            print("high number!!!!")   
+    
+    print("Great!!! The nunmber!!!!!")
+
+
+guess(10)    
